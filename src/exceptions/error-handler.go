bf2efc72be399298a7eb6768be9bf50dc3767602
razisyahputro <razisyahputro@gmail.com>
2024-11-02 17:50:34 +0700

    init project

---
+++ src/exceptions/error-handler.go
@@ -0,0 +1,25 @@
+package exception
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+func ValidationError(err interface{}) map[string]string {
+	validationErrors := err.(validator.ValidationErrors)
+	errorsMap := make(map[string]string)
+	for _, err := range validationErrors {
+		fieldName := err.Field()
+		switch err.Tag() {
+		case "required":
+			errorsMap[fieldName] = fieldName + " is required"
+		case "min":
+			errorsMap[fieldName] = fieldName + " must be at least " + err.Param() + " characters"
+		case "max":
+			errorsMap[fieldName] = fieldName + " must be at most " + err.Param() + " characters"
+		case "email":
+			errorsMap[fieldName] = "Invalid email format"
+		}
+	}
+
+	return errorsMap
+}
