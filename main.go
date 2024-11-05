88d02b3a243950f41bdd4a7dc1ed9ea74109a185
razisyahputro <razisyahputro@gmail.com>
2024-11-05 14:23:18 +0700

    implement env

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/razidev/movie-festival/src/configs"
 	"github.com/razidev/movie-festival/src/routers"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
 	configs.Connect()
 
 	server := http.Server{
--- src/configs/database.go
+++ src/configs/database.go
@@ -1,6 +1,9 @@
 package configs
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,7 +11,12 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "root:razidev@tcp(127.0.0.1:3306)/movie-festival?parseTime=True"
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True", dbUser, dbPass, dbHost, dbPort, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
